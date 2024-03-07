37029d8705fc5090be7f9882ec774089f40662b5
dreicouv <querubin@ualberta.ca>
2024-03-06 18:50:54 -0700

    Created Unit Test

---
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -0,0 +1,20 @@
+package com.example.lab8;
+
+public class CustomListTest {
+    /**
+     * this gets size of the list
+     * @return
+     */
+    public int getCount(){
+        return cities.size();
+    }
+
+    /**
+     * this adds a city object to the list
+     * for the first phase it will be empty
+     * @param city
+     */
+    public void addCity(City city){
+
+    }
+}
